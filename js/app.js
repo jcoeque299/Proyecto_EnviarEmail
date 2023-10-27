46d8d985c18f0797d446d3f7b81815d4f6e76fab
Judith <jcoeque299@g.educaand.es>
2023-10-27 12:58:44 +0200

    Actualizado

--- js/app.js
+++ js/app.js
@@ -10,10 +10,21 @@ inputMensaje.addEventListener("blur", validar)
 function validar(e) {
     if (e.target.value === "") {
         mostrarAlerta("El campo " + e.target.id + " no puede estar vacio", e.target.parentElement)
+        return
     }
-    else {
-        console.log("Todo OK")
+    if (e.target.value === "email" && !validarEmail(e.target.value)) {
+        mostrarAlerta("El email no es valido", e.target.parentElement)
+        return
     }
+    limpiarAlerta(e.target.parentElement)
+}
+
+function validarEmail(email) {
+    const validacion = new RegExp('^(.+)@(\\S+)$')
+    if (validacion.test(email)) {
+        return true
+    }
+    return false
 }
 
 function limpiarAlerta(referencia) {
